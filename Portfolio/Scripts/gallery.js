2de65698307e533bd29699d90ad38323b7d7192e
Daniel Poindexter <dp.texas@gmail.com>
2012-01-18 16:36:51 -0600

    Change error settings in web.config to detect AppHarbor errors

--- Portfolio/Scripts/gallery.js
+++ Portfolio/Scripts/gallery.js
@@ -30,14 +30,20 @@ function Gallery(images) {
         self.images.remove(img);
     }
 
-    self.moveNext = function(){
-        if (self.beforeLast())
+    self.moveNext = function () {
+        if (self.beforeLast()) {
             self.current(self.images()[self.current().ix() + 1]);
+            return true;
+        }
+        return false;
     }
 
-    self.movePrevious = function(){
-        if (self.afterFirst())
+    self.movePrevious = function () {
+        if (self.afterFirst()) {
             self.current(self.images()[self.current().ix() - 1]);
+            return true;
+        }
+        return false;
     }
 
     self.moveTo = function(img){
@@ -85,10 +91,12 @@ function Gallery(images) {
 
         //Add a listener for left and right arrow keys
         $(document).on("keydown", function (event) {
-            if (event.which === 39)
-                self.moveNext();
-            else if (event.which === 37)
-                self.movePrevious();
+            if (event.which === 39 && self.moveNext()) {
+                var pre = $("#back-key").attr("src")
+            }
+            else if (event.which === 37 && self.movePrevious()) {
+
+            }
         });
     } ()
 }
