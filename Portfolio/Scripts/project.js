917a59def0695f1987e42bea9ee1188e66f5ea35
Daniel Poindexter <dp.texas@gmail.com>
2012-01-19 02:53:13 -0600

    Include image files not linked to VS solution

---
+++ Portfolio/Scripts/project.js
@@ -0,0 +1,34 @@
+﻿function ProjectViewModel(images) {
+    var self = this;
+
+    self.images = ko.observableArray([]);
+
+    self.addImage = function (data) {
+        var currLen = self.images().length;
+        var data = _.defaults(data || {}, self.defaultImage(currLen));
+        self.images.push(data);
+    }
+
+    self.removeImage = function () {
+        self.images.remove(this);
+    }
+
+    self.defaultImage = function (num) {
+        return {
+            src: "",
+            caption: "",
+            srcFieldName: "Images[" + num + "].Src",
+            srcFieldId: "Images_" + num + "__Src",
+            captionFieldName: "Images[" + num + "].Caption",
+            captionFieldId: "Images_" + num + "__Caption"
+        };
+    }
+
+    init = function () {
+        if (_.isArray(images) && images.length > 0) {
+            _.each(images, function (img) {
+                self.addImage(img);
+            });
+        }
+    } ()
+}
